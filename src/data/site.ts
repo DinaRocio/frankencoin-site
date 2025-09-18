ce351beb5d2073a7d12f32bf211e16ac7ce5d5b0
dinarocio <dinarociovq15@hotmail.com>
2025-09-18 01:10:05 -0500

    feat: add button components and footer with social icons

---
+++ src/data/site.ts
@@ -0,0 +1,42 @@
+// src/data/site.ts
+export const site = {
+  name: "Frankencoin",
+  url: "https://frankencoin.com",
+  socials: [
+    {
+      name: "Telegram",
+      href: "https://t.me/frankencoinzchf",
+      icon: "src/assets/images/Social-icons-1_1.svg",
+      width: 30,
+      height: 30,
+    },
+    {
+      name: "X (Twitter)",
+      href: "https://x.com/frankencoinzchf",
+      icon: "src/assets/images/Social-icons_1.svg",
+      width: 30,
+      height: 30,
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/company/frankencoin/",
+      icon: "src/assets/images/Social-icons-2_1.svg",
+      width: 30,
+      height: 30,
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/Frankencoin-ZCHF/",
+      icon: "src/assets/images/Social-icons-4_1.svg",
+      width: 51,
+      height: 32,
+    },
+    {
+      name: "Docs (GitBook)",
+      href: "https://docs.frankencoin.com/",
+      icon: "src/assets/images/Gitbook_1.svg",
+      width: 30,
+      height: 30,
+    },
+  ],
+};
